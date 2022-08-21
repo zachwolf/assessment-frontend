e80513dd213accbe5d311ba401d45119bdb3d682
Zach Wolf <hello@zachwolf.com>
2022-08-21 16:10:49 -0500

    Static location

--- src/component/location/location.js
+++ src/component/location/location.js
@@ -1,5 +1,40 @@
 import Cell from "../../ui/cell";
+import Icon from "../../ui/icon";
+import styles from "./location.module.css";
 
-export default function Location() {
-    return <Cell>todo location</Cell>;
+export default function Location({ latitude, longitude }) {
+    return (
+        <Cell>
+            <div className={styles.location}>
+                <div className={styles.location__overview}>
+                    <div className={styles.todo}>
+                        <Icon type="cloud" scale={2} />
+                    </div>
+                    <div className={styles.todo}>
+                        <div className={styles.location__temperature}>
+                            78<abbr title="Fahrenheit">°F</abbr>
+                        </div>
+                        Cloudy
+                    </div>
+                </div>
+                <div className={styles.location__details}>
+                    <div className={styles.location__uhhhhLocation}>New York, NY</div>
+                    <div className={styles.location__upcoming}>
+                        <div className={styles.location__day}>
+                            <Icon type="cloud" />
+                            <abbr title="Friday">Fri</abbr>
+                        </div>
+                        <div className={styles.location__day}>
+                            <Icon type="cloud" />
+                            <abbr title="Saturday">Sat</abbr>
+                        </div>
+                        <div className={styles.location__day}>
+                            <Icon type="rain" />
+                            <abbr title="Sunday">Sun</abbr>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Cell>
+    );
 }
--- src/view/pageOneStatic.js
+++ src/view/pageOneStatic.js
@@ -2,14 +2,11 @@ import React from "react";
 import Image from "../ui/image/image";
 import Location from "../component/location";
 
-console.log("Image", Image);
-console.log("?");
-
 export default function PageOneStatic() {
     return (
         <div>
             <Image src="/img/shoes.png" alt="Phone displaying athletic shoe app" />
-            <Location />
+            <Location longitude="40.748607102729295" latitude="-73.98563758004718" />
         </div>
     );
 }
