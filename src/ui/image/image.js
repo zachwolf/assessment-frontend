24ebe6397f3cf6ff1c689f44169cffb1ac5fdbd1
Zach Wolf <hello@zachwolf.com>
2022-08-21 16:10:12 -0500

    console log cleanup

--- src/ui/image/image.js
+++ src/ui/image/image.js
@@ -2,10 +2,7 @@ import styles from "./image.module.css";
 import Cell from "../cell";
 
 export default function Image(props) {
-    console.log("props", props);
     const { src, alt, ...rest } = props;
-    console.log("src", src);
-    console.log("rest", rest);
     return (
         <Cell collapse className={styles.wrapper}>
             <span className={null}>
